0202fff3b1bc4c46fe627c8a835ee5451d3dc0ec
roxanapirlea <roxanap.dev@gmail.com>
2023-12-02 09:15:40 +0200

    Solve Day2 part 1

---
+++ src/Day02.kt
@@ -0,0 +1,51 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val maxCubes = Cubes(12, 13, 14)
+        return input.map { line ->
+            val (i, sets) = line.mapToSets()
+            val validSets = sets.map {
+                it.r <= maxCubes.r && it.g <= maxCubes.g && it.b <= maxCubes.b
+            }.all { it }
+            i to validSets
+        }
+            .filter { it.second }
+            .sumOf { it.first }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 8)
+
+    val input = readInput("Day02")
+    part1(input).println()
+}
+
+private fun String.mapToSets(): Pair<Int, List<Cubes>> {
+    val (i, sets) = substringAfter("Game ").split(":")
+    val cubesSets = sets.split(";").map { gameSet ->
+        gameSet.split(",").map {
+            Cubes(
+                it.trim().substringBefore(" red", "0").toInt(),
+                it.trim().substringBefore(" green", "0").toInt(),
+                it.trim().substringBefore(" blue", "0").toInt()
+            )
+        }.reduce { total, newCubes ->
+            Cubes(
+                total.r + newCubes.r,
+                total.g + newCubes.g,
+                total.b + newCubes.b,
+            )
+        }
+    }
+    return i.toInt() to cubesSets
+}
+
+private data class Cubes(
+    val r: Int,
+    val g: Int,
+    val b: Int
+)
